aeb78a49c86a4db48d9d08ec4d5aa8f518374ced
Fan Wang <nimadorostkar97@gmail.com>
2025-05-18 19:21:46 +0330

    debug JWT_SECRET

--- src/core/settings/common.py
+++ src/core/settings/common.py
@@ -10,6 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = environ.get("SECRET_KEY"),
+JWT_SECRET = environ.get("SECRET_KEY"),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
