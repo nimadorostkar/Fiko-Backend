d7bc6b94be9d4368d09fb6700f4fd6c469617331
Fan Wang <nimadorostkar97@gmail.com>
2025-05-19 09:41:07 +0330

    debug jwt 2

--- src/accounts/api/login.py
+++ src/accounts/api/login.py
@@ -7,5 +7,5 @@ class LoginAPIView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
