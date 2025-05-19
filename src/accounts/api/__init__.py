b985c5a88d08a8c5e2ab1ee50910a18bda605ace
Fan Wang <nimadorostkar97@gmail.com>
2025-05-19 20:41:30 +0330

    add responses and extentions

--- src/accounts/api/__init__.py
+++ src/accounts/api/__init__.py
@@ -1,2 +1,5 @@
 from accounts.api.login import LoginAPIView
 from accounts.api.register import RegisterView,CompleteRegisterView
+from accounts.api.refresh import Refresh
+from accounts.api.refresh_access import RefreshAccess
+from accounts.api.logout import Logout
--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from accounts.api import LoginAPIView,RegisterView,CompleteRegisterView
+from accounts.api import LoginAPIView,RegisterView,CompleteRegisterView,Refresh,RefreshAccess,Logout
 
 urlpatterns = [
     path("login", LoginAPIView.as_view(), name="login"),
     path('register', RegisterView.as_view(), name='register'),
     path('complete', CompleteRegisterView.as_view(), name='complete'),
+    path("refresh", Refresh.as_view(), name="refresh"),
+    path("refresh-access", RefreshAccess.as_view(), name="refresh-access"),
+    path("logout", Logout.as_view(), name="logout"),
 ]
