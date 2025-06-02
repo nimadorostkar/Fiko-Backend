728d1997ae0c88165a818dbabcf1ccf12a40a840
Fan Wang <nimadorostkar97@gmail.com>
2025-06-02 10:16:22 +0330

    history

--- src/billing/urls.py
+++ src/billing/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from billing.api import CurrentPlanAPIView,Payment,PaymentVerify#,PaymentHistory
+from billing.api import CurrentPlanAPIView,Payment,PaymentVerify,PaymentHistory
 
 urlpatterns = [
     path("current-plan", CurrentPlanAPIView.as_view(), name="current-plan"),
     path("payment", Payment.as_view(), name="payment"),
     path("payment-verify/<int:id>/", PaymentVerify.as_view(), name="payment-verify"),
-    #path("payment-history",PaymentHistory.as_view(),name="pay-history"),
+    path("payment-history",PaymentHistory.as_view(),name="pay-history"),
 ]
