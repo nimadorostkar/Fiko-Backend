fc27ea56ab4c8f0006f3f0b9b1eb3ec4eaccfddf
Fan Wang <nimadorostkar97@gmail.com>
2025-07-07 16:13:39 +0330

    connect-tele-bot 3

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from settings.views import PricesAPIView
 from settings.channels_view import ConnectTeleAPIView
 
+
 urlpatterns = [
     # channels
     path("connect-tele-bot", ConnectTeleAPIView.as_view(), name="connect-tele-bot"),
     # prices
     path("prices", PricesAPIView.as_view(), name="prices"),
 ]
+
+
