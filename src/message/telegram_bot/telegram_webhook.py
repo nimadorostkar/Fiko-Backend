956a229e77da6f5a6fdd6d65335e43d138da7e63
Fan Wang <nimadorostkar97@gmail.com>
2025-07-08 14:25:36 +0330

    TelegramWebhook updated

--- src/message/telegram_bot/telegram_webhook.py
+++ src/message/telegram_bot/telegram_webhook.py
@@ -7,7 +7,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny,IsAuthenticated
-from message.models import Customer,Conversation
+from message.models import Customer,Conversation,Message
 from settings.models import TelegramChannel
 
 
@@ -27,7 +27,7 @@ class TelegramWebhook(APIView):
             channel = TelegramChannel.objects.get(bot_username=bot_name)
             bot_user = channel.user
 
-            # Create or update Customer
+            # Create or update Customer -----------------
             customer, created = Customer.objects.update_or_create(
                 source='telegram',source_id=telegram_id,
                 defaults={'first_name':first_name,'last_name':last_name,}
@@ -35,19 +35,21 @@ class TelegramWebhook(APIView):
             action = "created" if created else "updated"
             print(f"Customer {action}: {customer}")
 
-            # Create or update Conversation
+            # Create or update Conversation -----------------
             conversation, created = Conversation.objects.update_or_create(
                 user=bot_user,source='telegram', customer=customer,
             )
             action = "created" if created else "updated"
             print(f"Conversation {action}: {conversation}")
 
+            # Create Message -----------------
+            message = Message.objects.create(content=message_text, conversation=conversation, customer=customer,)
+            print(f"Message created: {message}")
 
             # You can now process or store this message, or send it over WebSocket
             # Optional: respond back
             # send_telegram_message(chat_id, "Received your message!")
+
             return JsonResponse({"status": "ok"})
         except Exception as e:
             return Response(f"Error: {str(e)}", status=status.HTTP_400_BAD_REQUEST)
-
-
